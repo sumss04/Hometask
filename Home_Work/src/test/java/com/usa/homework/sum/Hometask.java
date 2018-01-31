95915df3c35578c041a93cadf6d2bdb0f635e588
Mohammad Ali <Mohammad Ali@DESKTOP-SVCS4KF>
2018-01-30 23:26:13 -0500

    Version 0.07

---
+++ Home_Work/src/test/java/com/usa/homework/sum/Hometask.java
@@ -0,0 +1,24 @@
+package com.usa.homework.sum;
+
+	public class Hometask {
+	public static String name(){
+		System.out.println("Bangladesh");
+		return null;	
+	}
+	
+	public static void salary(){
+		System.out.println("25000");
+	}
+	public void income() {
+		System.out.println("10000");
+	}
+	
+public static void main(String[] args) {
+	
+	Hometask.name();
+	Hometask.salary();
+	Hometask obj =new Hometask();
+	obj.income();
+	
+}
+}
